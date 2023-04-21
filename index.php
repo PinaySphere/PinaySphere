710d5fb32daf3c5035bfdbd7311ffd36d4507555
PinaySphere <129018551+PinaySphere@users.noreply.github.com>
2023-04-21 13:43:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+    crossorigin="anonymous" 
+    referrerpolicy="no-referrer" />
+    echo include_once (dirname(__FILE__) . '/pa_antiadblock_(5863280).php');
+    <title>PinaySphere</title>
+</head>
+<body>
+
+    <header>
+        <div class="navbar">
+            <div class="top">
+                <a href="#">
+                    <img class="logo" src="assets/Logo.png"></img>
+                </a>
+                <div class="socmed-icon">
+                    <a href="https://facebook.com/PinaySphereFB" title="https://facebook.com/PinaySphereFB" target="_blank"><i class="fa-brands fa-facebook-f"></i></i></a>
+                    <a href="https://twitter.com/PinaySphereTW" title="https://twitter.com/PinaySphereTW" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="https://t.me/PinaySphere" title="https://t.me/PinaySphere" target="_blank"><i class="fab fa-telegram"></i></a>
+                    <a href="https://discord.gg/dPkZZZFfeZ" title="https://discord.gg/dPkZZZFfeZ" target="_blank"><i class="fa-brands fa-discord"></i></a>
+                </div>
+                <ul class="links">
+                    <li><a href="home.html">Home</a></li>
+                    <li><a href="categories.html">Categories</a></li>
+                    <li><a href="about-us.html">About</a></li>
+                    <li><a href="contact-us.html">Contact us</a></li>
+                </ul>
+                <a class="action_button" href="donate.html">Donate</a>
+                <div class="toggle_button">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
+            </div>
+
+            <div class="dropdown_menu">
+                <li><a href="home.html">Home</a></li>
+                <li><a href="categories.html">Categories</a></li>
+                <li><a href="about-us.html">About</a></li>
+                <li><a href="contact-us.html">Contact us</a></li>
+                <li><a class="action_button" href="donate.html">Donate</a></li>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="main-content">
+            <div class="grid">
+                <div class="grid-container"><a href="watch/first-video-namin-ng-partner-kong-may-pink-at-matambok-na-puke/first-video-namin-ng-partner-kong-may-pink-at-matambok-na-puke.html"><img class="grid-image" src="watch/first-video-namin-ng-partner-kong-may-pink-at-matambok-na-puke/first-video-namin-ng-partner-kong-may-pink-at-matambok-na-puke.png" alt=""><h1>First video namin ng Partner kong may Pink at Matambok na Puke</h1></a></div>
+                <div class="grid-container"><a href="watch/doggy-malala-sa-nakilala-ko-sa-tinder-app-baka-kilala-nyo-to/doggy-malala-sa-nakilala-ko-sa-tinder-app-baka-kilala-nyo-to.html"><img class="grid-image" src="watch/doggy-malala-sa-nakilala-ko-sa-tinder-app-baka-kilala-nyo-to/doggy-malala-sa-nakilala-ko-sa-tinder-app-baka-kilala-nyo-to.png" alt=""><h1>Doggy malala sa nakilala ko sa Tinder App baka kilala nyo to</h1></a></div>
+                <div class="grid-container"><a href="watch/ako-pa-nga-naka-virgin-sa-inaanak-ko-bilis-ng-panahon/ako-pa-nga-naka-virgin-sa-inaanak-ko-bilis-ng-panahon.html"><img class="grid-image" src="watch/ako-pa-nga-naka-virgin-sa-inaanak-ko-bilis-ng-panahon/ako-pa-nga-naka-virgin-sa-inaanak-ko-bilis-ng-panahon.png" alt=""><h1>Ako pa nga naka virgin sa Inaanak ko bilis ng panahon</h1></a></div>
+                <div class="grid-container"><a href="watch/student-at-professor-kantutan-sa-sementeryo/student-at-professor-kantutan-sa-sementeryo.html"><img class="grid-image" src="watch/student-at-professor-kantutan-sa-sementeryo/student-at-professor-kantutan-sa-sementeryo.png" alt=""><h1>Student at Professor kantutan sa Sementeryo</h1></a></div>
+                <div class="grid-container"><a href="watch/student-hindi-binayaran-matapos-ang-kantutan/student-hindi-binayaran-matapos-ang-kantutan.html"><img class="grid-image" src="watch/student-hindi-binayaran-matapos-ang-kantutan/student-hindi-binayaran-matapos-ang-kantutan.png" alt=""><h1>Student hindi binayaran matapos ang Kantutan</h1></a></div>
+                <div class="grid-container"><a href="watch/nagpakantot-ako-sa-tropa-ng-jowa-ko/nagpakantot-ako-sa-tropa-ng-jowa-ko.html"><img class="grid-image" src="watch/nagpakantot-ako-sa-tropa-ng-jowa-ko/nagpakantot-ako-sa-tropa-ng-jowa-ko.png" alt=""><h1>Nagpakantot ako sa Tropa ng Jowa ko</h1></a></div>
+                <div class="grid-container"><a href="watch/kantot-habang-tulog/kantot-habang-tulog.html"><img class="grid-image" src="watch/kantot-habang-tulog/kantot-habang-tulog.png" alt=""><h1>Kantot habang tulog</h1></a></div>
+                <div class="grid-container"><a href="watch/naka-dalawang-putok-si-tito-grabe/naka-dalawang-putok-si-tito-grabe.html"><img class="grid-image" src="watch/naka-dalawang-putok-si-tito-grabe/naka-dalawang-putok-si-tito-grabe.png" alt=""><h1>Naka dalawang putok si Tito grabe</h1></a></div>
+                <div class="grid-container"><a href="muntik-na-kame-mahuli-sa-kotse/muntik-na-kame-mahuli-sa-kotse.html"><img class="grid-image" src="watch/muntik-na-kame-mahuli-sa-kotse/muntik-na-kame-mahuli-sa-kotse.png" alt=""><h1>Muntik na kame Mahuli sa Kotse</h1></a></div>
+                <div class="grid-container"><a href="watch/kantutan-sa-school/kantutan-sa-school.html"><img class="grid-image" src="watch/kantutan-sa-school/kantutan-sa-school.png" alt=""><h1>Kantutan sa School</h1></a></div>
+                <div class="grid-container"><a href="watch/malaman-si-marites/malaman-si-marites.html"><img class="grid-image" src="watch/malaman-si-marites/malaman-si-marites.png" alt=""><h1>Malaman si Marites</h1></a></div>
+                <div class="grid-container"><a href="watch/ang-giling-ni-maria/ang-giling-ni-maria.html"><img class="grid-image" src="watch/ang-giling-ni-maria/ang-giling-ni-maria.png" alt=""><h1>Ang Giling ni Maria</h1></a></div>
+                <div class="grid-container"><a href="watch/nakalimutan-ata-nilang-magkapatid-sila/nakalimutan-ata-nilang-magkapatid-sila.html"><img class="grid-image" src="watch/nakalimutan-ata-nilang-magkapatid-sila/nakalimutan-ata-nilang-magkapatid-sila.png" alt=""><h1>Nakalimutan ata nilang Magkapatid sila</h1></a></div>
+                <div class="grid-container"><a href="watch/pinsan-kong-lasing-nagpakantot-sakin/pinsan-kong-lasing-nagpakantot-sakin.html"><img class="grid-image" src="watch/pinsan-kong-lasing-nagpakantot-sakin/pinsan-kong-lasing-nagpakantot-sakin.png" alt=""><h1>Pinsan kong Lasing Nagpakantot sakin</h1></a></div>
+                <div class="grid-container"><a href="watch/winasak-ko-ang-kepyas-ni-gina/winasak-ko-ang-kepyas-ni-gina.html"><img class="grid-image" src="watch/winasak-ko-ang-kepyas-ni-gina/winasak-ko-ang-kepyas-ni-gina.png" alt=""><h1>Winasak ko ang kepyas ni Gina</h1></a></div>
+                <div class="grid-container"><a href="watch/petit-ang-aking-nais/petit-ang-aking-nais.html"><img class="grid-image" src="watch/petit-ang-aking-nais/petit-ang-aking-nais.png" alt=""><h1>Petit ang Aking nais</h1></a></div>
+                <div class="grid-container"><a href="watch/kantutan-sa-damuhan/kantutan-sa-damuhan.html"><img class="grid-image" src="watch/kantutan-sa-damuhan/kantutan-sa-damuhan.png" alt=""><h1>Kantutan sa damuhan</h1></a></div>
+                <div class="grid-container"><a href="watch/ang-pagbigay-ng-regalo-kay-pia/ang-pagbigay-ng-regalo-kay-pia.html"><img class="grid-image" src="watch/ang-pagbigay-ng-regalo-kay-pia/ang-pagbigay-ng-regalo-kay-pia.png" alt=""><h1>Ang pagbigay ng regalo kay pia</h1></a></div>
+                <div class="grid-container"><a href="watch/iyot-sa-kotse/iyot-sa-kotse.html"><img class="grid-image" src="watch/iyot-sa-kotse/iyot-sa-kotse.png" alt=""><h1>Iyot sa Kotse</h1></a></div>
+                <div class="grid-container"><a href="watch/giling-ni-kurimaw/giling-ni-kurimaw.html"><img class="grid-image" src="watch/giling-ni-kurimaw/giling-ni-kurimaw.png" alt=""><h1>Giling ni kurimaw</h1></a></div>
+                <div class="grid-container"><a href="watch/jackpot-pinaisa-ako-ni-step-sis/jackpot-pinaisa-ako-ni-step-sis.html"><img class="grid-image" src="watch/jackpot-pinaisa-ako-ni-step-sis/jackpot-pinaisa-ako-ni-step-sis.png" alt=""><h1>Jackpot pinaisa ako ni step sis</h1></a></div>
+                <div class="grid-container"><a href="watch/kantot-muna-bago-laro-kasi/kantot-muna-bago-laro-kasi.html"><img class="grid-image" src="watch/kantot-muna-bago-laro-kasi/kantot-muna-bago-laro-kasi.png" alt=""><h1>Kantot muna bago laro kasi</h1></a></div>
+                <div class="grid-container"><a href="watch/iba-lasa-ni-girl-bestfriend/iba-lasa-ni-girl-bestfriend.html"><img class="grid-image" src="watch/iba-lasa-ni-girl-bestfriend/iba-lasa-ni-girl-bestfriend.png" alt=""><h1>Iba lasa ni girl bestfriend</h1></a></div>
+                <div class="grid-container"><a href="watch/kulitan-with-my-klasmeyt/kulitan-with-my-klasmeyt.html"><img class="grid-image" src="watch/kulitan-with-my-klasmeyt/kulitan-with-my-klasmeyt.png" alt=""><h1>Kulitan with my Klasmeyt</h1></a></div>
+                <div class="grid-container"><a href="watch/pakantot-muna-sa-jowa-ni-mister/pakantot-muna-sa-jowa-ni-mister.html"><img class="grid-image" src="watch/pakantot-muna-sa-jowa-ni-mister/pakantot-muna-sa-jowa-ni-mister.png" alt=""><h1>Pakantot muna sa jowa ni mister</h1></a></div>
+                <div class="grid-container"><a href="watch/sexy-namin-ng-pinsan-ko/sexy-namin-ng-pinsan-ko.html"><img class="grid-image" src="watch/sexy-namin-ng-pinsan-ko/sexy-namin-ng-pinsan-ko.png" alt=""><h1>Sexy namin ng pinsan ko</h1></a></div>
+                <div class="grid-container"><a href="watch/grace-lara-scandal/grace-lara-scandal.html"><img class="grid-image" src="watch/grace-lara-scandal/grace-lara-scandal.png" alt=""><h1>Grace Lara Scandal</h1></a></div>
+                <div class="grid-container"><a href="watch/nakuha-kong-850-pesos-lang-sa-quiapo/nakuha-kong-850-pesos-lang-sa-quiapo.html"><img class="grid-image" src="watch/nakuha-kong-850-pesos-lang-sa-quiapo/nakuha-kong-850-pesos-lang-sa-quiapo.png" alt=""><h1>Nakuha kong 850 pesos lang sa quiapo</h1></a></div>
+                <div class="grid-container"><a href="watch/sarap-na-sarap-sa-dede-ko-si-kuya/sarap-na-sarap-sa-dede-ko-si-kuya.html"><img class="grid-image" src="watch/sarap-na-sarap-sa-dede-ko-si-kuya/sarap-na-sarap-sa-dede-ko-si-kuya.png" alt=""><h1>Sarap na sarap sa dede ko si kuya</h1></a></div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="content">
+          <div class="top">
+            <div class="logo-details">
+              <img class="logo_name" src="assets/Logo.png">
+            </div>
+            <div class="media-icons">
+              <a href="https://www.facebook.com/PinaySphereFB" title="https://www.facebook.com/PinaySphereFB" target="_blank"><i class="fa-brands fa-facebook-f"></i></i></a>
+              <a href="https://twitter.com/PinaySphereTW" title="https://twitter.com/PinaySphereTW" target="_blank"><i class="fab fa-twitter"></i></a>
+              <a href="https://t.me/PinaySphere" title="https://t.me/PinaySphere" target="_blank"><i class="fab fa-telegram"></i></a>
+              <a href="https://discord.gg/dPkZZZFfeZ" title="https://discord.gg/dPkZZZFfeZ" target="_blank"><i class="fa-brands fa-discord"></i></a>
+            </div>
+          </div>
+          <div class="link-boxes">
+            <ul class="box">
+              <li class="link_name">Quick Links</li>
+              <li><a href="home.html">Home</a></li>
+              <li><a href="categories.html">Categories</a></li>
+              <li><a href="contact-us.html">Contact us</a></li>
+              <li><a href="about-us.html">About us</a></li>
+              
+            </ul>
+            <ul class="box">
+              <li class="link_name">Other Sites</li>
+              <li><a href="https://pornsphere.com/" title="https://pornsphere.com/">PornSphere.com</a></li>
+              <li><a href="https://fansphere.com/" title="https://fansphere.com/">Fansphere.com</a></li>
+            </ul>
+            <ul class="box">
+              <li class="link_name">Community</li>
+              <li><a href="https://facebook.com/PinaySphereFB" title="https://facebook.com/PinaySphereFB" target="_blank">Facebook</a></li>
+              <li><a href="https://twitter.com/PinaySphereTW" title="https://twitter.com/PinaySphereTW" target="_blank">Twitter</a></li>
+              <li><a href="https://t.me/PinaySphere" title="https://t.me/PinaySphere" target="_blank">Telegram</a></li>
+              <li><a href="https://discord.gg/dPkZZZFfeZ" title="https://discord.gg/dPkZZZFfeZ" target="_blank">Discord</a></li>
+            </ul>
+            <ul class="box">
+              <li class="link_name">PinaySphere</li>
+              <li><a href="dmca.html">DMCA</a></li>
+              <li><a href="privacy-policy.html">Privacy Policy</a></li>
+              <li><a href="term-of-service.html">Terms of Service</a></li>
+            </ul>
+            <ul class="box input-box">
+              <li class="link_name">Subscribe</li>
+              <li><input type="text" placeholder="Enter your email"></li>
+              <li><input type="button" value="Subscribe"></li>
+            </ul>
+          </div>
+        </div>
+        <div class="bottom-details">
+          <div class="bottom_text">
+            <span class="copyright_text">Copyright © 2023 <a href="home.html">PinaySphere.</a>All rights reserved</span>
+            <span class="policy_terms">
+              <a href="privacy-policy.html">Privacy Policy</a>
+              <a href="term-of-service.html">Terms of Service</a>
+            </span>
+          </div>
+        </div>
+      </footer>
+
+    <script src="assets/js/navbar.js"></script>
+
+</body>
+</html>
